7b518a4ad09954f966d46bc41d86975fb32fe82d
Bence_Simon <simon.bence45@gmail.com>
2022-09-06 14:26:17 +0200

    My second commit

--- src/App.java
+++ src/App.java
@@ -2,8 +2,9 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello world");
 
+        System.out.println("Another one");
     }
 
 }
