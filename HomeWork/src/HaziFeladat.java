2fa27adceff9b6fac759fdf0648d56558c3b1cc6
SimonBence95 <112952826+SimonBence95@users.noreply.github.com>
2022-09-08 12:08:12 +0200

    Add files via upload
    Homework

---
+++ HomeWork/src/HaziFeladat.java
@@ -0,0 +1,11 @@
+public class HaziFeladat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ez itt a hazi feladat");
+
+        System.out.println("Ez a szoveg a plusz commit miatt szuksegeltetik");
+
+    }
+
+}
